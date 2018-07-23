4684dc1ead478e6b2e416153b4bba2ae437325e2
William Barbosa <william.sb@hotmail.com>
2018-07-23 15:15:45 -0300

    :construction: Make calendar fragment reactive

---
+++ Toggl.Giskard/Fragments/ReportsCalendarFragment.ViewBinds.cs
@@ -0,0 +1,31 @@
+﻿using Android.Support.V4.View;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using static Toggl.Giskard.Resource.Id;
+
+namespace Toggl.Giskard.Fragments
+{
+    public sealed partial class ReportsCalendarFragment
+    {
+        private ViewPager pager;
+
+        private LinearLayout header;
+
+        private TextView calendarCurrentMonthTextView;
+
+        private RecyclerView shortcutsRecyclerView;
+
+        protected override void InitializeViews(View view)
+        {
+            pager = view.FindViewById<ViewPager>(ReportsCalendarFragmentViewPager);
+
+            header = view.FindViewById<LinearLayout>(ReportsCalendarFragmentHeader);
+
+            shortcutsRecyclerView = view.FindViewById<RecyclerView>(ReportsCalendarFragmentShortcuts);
+
+            calendarCurrentMonthTextView = view.FindViewById<TextView>(ReportsCalendarCurrentMonthTitle);
+
+        }
+    }
+}
--- Toggl.Giskard/Fragments/ReportsCalendarFragment.cs
+++ Toggl.Giskard/Fragments/ReportsCalendarFragment.cs
@@ -1,116 +1,89 @@
-﻿using System;
-using System.ComponentModel;
-using System.Linq;
-using System.Reactive.Disposables;
+﻿using System.Collections.Immutable;
+using System.Reactive.Linq;
 using Android.OS;
-using Android.Support.V4.View;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
-using MvvmCross;
 using MvvmCross.Binding.BindingContext;
-using MvvmCross.Droid.Support.V4;
-using MvvmCross.Droid.Support.V7.RecyclerView;
-using MvvmCross.Platforms.Android.Binding.BindingContext;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
-using MvvmCross.WeakSubscription;
-using Toggl.Foundation.MvvmCross.Parameters;
 using Toggl.Foundation.MvvmCross.ViewModels;
 using Toggl.Giskard.Activities;
 using Toggl.Giskard.Adapters;
 using Toggl.Giskard.Extensions;
+using Toggl.Giskard.ViewHolders;
 using Toggl.Multivac.Extensions;
 
 namespace Toggl.Giskard.Fragments
 {
     [MvxFragmentPresentation(typeof(ReportsViewModel), Resource.Id.ReportsCalendarContainer, AddToBackStack = false)]
-    public sealed class ReportsCalendarFragment : MvxFragment<ReportsCalendarViewModel>
+    public sealed partial class ReportsCalendarFragment : ReactiveFragment<ReportsCalendarViewModel>
     {
         private int rowHeight;
-        private ViewPager pager;
-        private int currentRowCount;
-        private CompositeDisposable disposableBag = new CompositeDisposable();
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
-            var view = this.BindingInflate(Resource.Layout.ReportsCalendarFragment, null);
+            var view = InflateAndInitializeViews(Resource.Layout.ReportsCalendarFragment);
 
             rowHeight = Activity.Resources.DisplayMetrics.WidthPixels / 7;
 
-            Mvx.Resolve<IMvxBindingContextStack<IMvxAndroidBindingContext>>()
-               .Push(BindingContext as IMvxAndroidBindingContext);
+            var currentMonthObservable = ViewModel.CurrentMonthName.CombineLatest(ViewModel.CurrentYear, formatMonthAndYear);
 
-            pager = view.FindViewById<ViewPager>(Resource.Id.ReportsCalendarFragmentViewPager);
-            pager.Adapter = new CalendarPagerAdapter(Activity, ViewModel);
-            pager.SetCurrentItem(ViewModel.Months.Count - 1, false);
+            var calendarAdapter = new CalendarPagerAdapter(Activity, ViewModel.CalendarDayTapped);
+            pager.Adapter = calendarAdapter;
+            pager.SetCurrentItem(11, false);
 
-            view.FindViewById<MvxRecyclerView>(Resource.Id.ReportsCalendarFragmentShortcuts)
+            var shortcutAdapter = new SimpleAdapter<QuickSelectShortcut>(
+                Resource.Layout.ReportsCalendarShortcutCell,
+                QuickSelectShortcutViewHolder.Create
+            );
+
+            shortcutAdapter.OnItemTapped = ViewModel.QuickSelect;
+            shortcutsRecyclerView.SetAdapter(shortcutAdapter);
+            shortcutsRecyclerView
                 .SetLayoutManager(new LinearLayoutManager(Activity, LinearLayoutManager.Horizontal, false));
 
-            view.FindViewById<LinearLayout>(Resource.Id.ReportsCalendarFragmentHeader)
-                .GetChildren<TextView>()
-                .Indexed()
-                .ForEach((textView, index)
-                    => textView.Text = ViewModel.DayHeaderFor(index));
+            // Text
+            this.Bind(ViewModel.DayHeaders, bindHeaders);
+            this.Bind(currentMonthObservable, calendarCurrentMonthTextView.BindText());
 
-            ViewModel.WeakSubscribe<PropertyChangedEventArgs>(nameof(ViewModel.RowsInCurrentMonth), onRowCountChanged)
-                .DisposedBy(disposableBag);
+            // Shortcuts
+            this.Bind(ViewModel.QuickSelectShortcuts, shortcutAdapter.BindItems());
 
-            ViewModel.SelectedDateRangeObservable.Subscribe(onDateRangeChanged)
-                .DisposedBy(disposableBag);
+            // Calendar
+            this.Bind(ViewModel.Months, months => calendarAdapter.Months = months);
+            this.Bind(ViewModel.CurrentPage, pager.BindCurrentPage());
+            this.Bind(pager.CurrentPage(), ViewModel.OnCurrentPageChanged);
+            this.Bind(ViewModel.RowsInCurrentMonth, recalculatePagerHeight);
 
-            recalculatePagerHeight();
+            this.BindVoid(ViewModel.ReloadCalendar, () =>
+            {   
+                shortcutAdapter.NotifyDataSetChanged();
+                calendarAdapter.RefreshPage(pager.CurrentItem);
+            });
 
             return view;
-        }
 
-        private void onDateRangeChanged(DateRangeParameter dateRange)
-        {
-            var anyShortcutIsSelected = ViewModel.QuickSelectShortcuts.Any(shortcut => shortcut.Selected);
-            if (!anyShortcutIsSelected) return;
+            string formatMonthAndYear(string month, string year)
+                => $"{month} {year}";
 
-            var dateRangeStartDate = dateRange.StartDate;
-            var monthToScroll = ViewModel.Months.IndexOf(month => month.CalendarMonth.Month == dateRangeStartDate.Month);
-            if (monthToScroll == pager.CurrentItem) return;
-
-            var dateRangeStartDateIsContaintedInCurrentMonthView = ViewModel
-                .Months[pager.CurrentItem]
-                .Days.Any(day => day.DateTimeOffset == dateRangeStartDate);
-
-            if (!dateRangeStartDateIsContaintedInCurrentMonthView || dateRangeStartDate.Month == dateRange.EndDate.Month)
+            void bindHeaders(IImmutableList<string> headers)
             {
-                pager.SetCurrentItem(monthToScroll, true);
+                header.GetChildren<TextView>()
+                    .Indexed()
+                    .ForEach((textView, index)
+                        => textView.Text = headers[index]);
             }
-        }
 
-        private void onRowCountChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (currentRowCount == ViewModel.RowsInCurrentMonth)
-                return;
+            void recalculatePagerHeight(int rowCount)
+            {
+                var layoutParams = pager.LayoutParameters;
+                layoutParams.Height = rowHeight * rowCount;
+                pager.LayoutParameters = layoutParams;
 
-            recalculatePagerHeight();
-        }
-
-        private void recalculatePagerHeight()
-        {
-            currentRowCount = ViewModel.RowsInCurrentMonth;
-
-            var layoutParams = pager.LayoutParameters;
-            layoutParams.Height = rowHeight * currentRowCount;
-            pager.LayoutParameters = layoutParams;
-
-            var activity = (ReportsActivity)Activity;
-            activity.RecalculateCalendarHeight();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-
-            if (!disposing || disposableBag == null) return;
-
-            disposableBag.Dispose();
+                var activity = (ReportsActivity)Activity;
+                activity.RecalculateCalendarHeight();
+            }
         }
     }
 }
