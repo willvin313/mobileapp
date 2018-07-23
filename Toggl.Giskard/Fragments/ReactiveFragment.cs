61cc82016c14c89ee391935cd31769fa588e428a
William Barbosa <william.sb@hotmail.com>
2018-07-23 15:14:31 -0300

    :construction: Add basic reactive fragment

---
+++ Toggl.Giskard/Fragments/ReactiveFragment.cs
@@ -0,0 +1,30 @@
+﻿using System.Reactive.Disposables;
+using Android.Views;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.ViewModels;
+using Toggl.Foundation.MvvmCross.ViewModels;
+
+namespace Toggl.Giskard.Fragments
+{
+    public abstract class ReactiveFragment<TViewModel> : MvxFragment<TViewModel>, IReactiveBindingHolder
+        where TViewModel : MvxViewModel
+    {
+        public CompositeDisposable DisposeBag { get; } = new CompositeDisposable();
+
+        protected View InflateAndInitializeViews(int resourceId)
+        {
+            var view = LayoutInflater.Inflate(resourceId, null);
+            InitializeViews(view);
+            return view;
+        }
+
+        protected abstract void InitializeViews(View view);
+
+        public override void OnDestroyView()
+        {
+            base.OnDestroyView();
+
+            DisposeBag?.Dispose();
+        }
+    }
+}
