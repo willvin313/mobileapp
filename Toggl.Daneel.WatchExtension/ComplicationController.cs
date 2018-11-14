019cab18ec7678af6fcad43d66241e90753fa5e4
Juan Laube <juanlaube@icloud.com>
2018-11-14 16:00:39 -0300

    :watch: Adds modular large complication family

--- Toggl.Daneel.WatchExtension/ComplicationController.cs
+++ Toggl.Daneel.WatchExtension/ComplicationController.cs
@@ -3,6 +3,7 @@
 using Foundation;
 using ClockKit;
 using WatchConnectivity;
+using UIKit;
 
 namespace Toggl.Daneel.WatchExtension
 {
@@ -44,6 +45,44 @@ namespace Toggl.Daneel.WatchExtension
                     handler(entry);
                 }
             }
+            if (complication.Family == CLKComplicationFamily.ModularLarge)
+            {
+                var runningTimeEntry = WCSession.DefaultSession.ReceivedApplicationContext["RunningTimeEntry"] as NSDictionary;
+                if (runningTimeEntry != null)
+                {
+                    var description = (runningTimeEntry["Description"] as NSString).ToString();
+                    description = description.Length > 0 ? description : "No description";
+
+                    var startTime = runningTimeEntry["Start"] as NSDate;
+
+                    var template = new CLKComplicationTemplateModularLargeStandardBody();
+                    var descriptionTextProvider = CLKSimpleTextProvider.FromText(description, "Toggl");
+                    descriptionTextProvider.TintColor = UIColor.Red;
+                    var projectTextProvider = CLKSimpleTextProvider.FromText("No project", "Toggl");
+                    projectTextProvider.TintColor = UIColor.LightGray;
+                    var runningTimeTextProvider = CLKRelativeDateTextProvider.FromDate(startTime, CLKRelativeDateStyle.Timer, NSCalendarUnit.Hour | NSCalendarUnit.Minute | NSCalendarUnit.Second);
+                    runningTimeTextProvider.TintColor = UIColor.White;
+                    template.HeaderTextProvider = descriptionTextProvider;
+                    template.Body1TextProvider = projectTextProvider;
+                    template.Body2TextProvider = runningTimeTextProvider;
+
+                    var entry = CLKComplicationTimelineEntry.Create(NSDate.Now, template);
+                    handler(entry);
+                }
+                else
+                {
+                    var template = new CLKComplicationTemplateModularLargeStandardBody();
+                    var headerTextProvider = CLKSimpleTextProvider.FromText("Toggl", "Toggl");
+                    headerTextProvider.TintColor = UIColor.Red;
+                    var subheaderTextProvider = CLKSimpleTextProvider.FromText("Start time entry", "Toggl");
+                    subheaderTextProvider.TintColor = UIColor.White;
+                    template.HeaderTextProvider = headerTextProvider;
+                    template.Body1TextProvider = subheaderTextProvider;
+
+                    var entry = CLKComplicationTimelineEntry.Create(NSDate.Now, template);
+                    handler(entry);
+                }
+            }
         }
 
         public override void GetLocalizableSampleTemplate(CLKComplication complication, Action<CLKComplicationTemplate> handler)
@@ -54,6 +93,18 @@ namespace Toggl.Daneel.WatchExtension
                 var template = new CLKComplicationTemplateUtilitarianLargeFlat();
                 var textProvider = CLKSimpleTextProvider.FromText("Hello, World!", "Toggl");
                 template.TextProvider = textProvider;
+
+                handler(template);
+            }
+            if (complication.Family == CLKComplicationFamily.ModularLarge)
+            {
+                var template = new CLKComplicationTemplateModularLargeStandardBody();
+                var descriptionTextProvider = CLKSimpleTextProvider.FromText("Hello, World!", "Toggl");
+                var projectTextProvider = CLKSimpleTextProvider.FromText("No project", "Toggl");
+                var runningTimeTextProvider = CLKTimeTextProvider.FromDate(NSDate.Now);
+                template.HeaderTextProvider = descriptionTextProvider;
+                template.Body1TextProvider = projectTextProvider;
+                template.Body2TextProvider = runningTimeTextProvider;
 
                 handler(template);
             }
