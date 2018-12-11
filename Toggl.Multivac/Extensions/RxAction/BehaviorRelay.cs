9b41ffbcb7196a156a500553496a7f13a123186a
khoi <khoiracle@gmail.com>
2018-12-11 11:41:15 +0700

    Add BehaviorRelay reactive class

---
+++ Toggl.Multivac/Extensions/RxAction/BehaviorRelay.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Reactive.Subjects;
+
+namespace Toggl.Multivac.Extensions.Reactive
+{
+    public class BehaviorRelay<T> : IObservable<T>
+    {
+        private BehaviorSubject<T> internalSubject;
+
+        public BehaviorRelay(T value)
+        {
+            internalSubject = new BehaviorSubject<T>(value);
+        }
+
+        public void Accept(T value)
+        {
+            internalSubject.OnNext(value);
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return internalSubject.Subscribe(observer);
+        }
+    }
+}
