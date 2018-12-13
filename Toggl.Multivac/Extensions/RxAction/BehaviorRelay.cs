6b1539b64709060debc428302fbe807d1fecb9ed
khoi <khoiracle@gmail.com>
2018-12-13 11:24:00 +0700

    Add doc for BehaviorRelay

--- Toggl.Multivac/Extensions/RxAction/BehaviorRelay.cs
+++ Toggl.Multivac/Extensions/RxAction/BehaviorRelay.cs
@@ -3,6 +3,11 @@ using System.Reactive.Subjects;
 
 namespace Toggl.Multivac.Extensions.Reactive
 {
+    /// <summary>
+    /// A wrapper for BehaviorSubject.
+    /// It behaves just like BehaviorSubject, except that it can't terminate or error out.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements processed by the subject.</typeparam>
     public class BehaviorRelay<T> : IObservable<T>
     {
         private BehaviorSubject<T> internalSubject;
