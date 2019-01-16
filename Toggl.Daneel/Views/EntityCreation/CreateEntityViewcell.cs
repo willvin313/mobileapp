1ac853583022a5cf5ca4abcde0e0d753992ba6f2
Arturs Eimanis <arturseimanis@gmail.com>
2019-01-16 17:38:23 +0200

    :art: Add CreateEntityViewcell

---
+++ Toggl.Daneel/Views/EntityCreation/CreateEntityViewcell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Foundation;
+using Toggl.Daneel.Cells;
+using Toggl.Daneel.Extensions;
+using UIKit;
+
+namespace Toggl.Daneel.Views.EntityCreation
+{
+    public sealed partial class CreateEntityViewcell : BaseTableViewCell<string>
+    {
+        public static readonly NSString Key = new NSString(nameof(CreateEntityViewcell));
+        public static readonly UINib Nib;
+
+        static CreateEntityViewcell()
+        {
+            Nib = UINib.FromName(nameof(CreateEntityViewcell), NSBundle.MainBundle);
+        }
+
+        protected CreateEntityViewcell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        protected override void UpdateView()
+        {
+            TextLabel.AttributedText = Item.PrependWithAddIcon(TextLabel.Font.CapHeight);
+        }
+    }
+}
+
