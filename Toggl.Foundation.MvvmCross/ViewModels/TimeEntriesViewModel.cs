92bce4c240b241cf2b825473b3ad62d14a12f128
Simon Rozsival <simon@rozsival.com>
2019-03-22 10:41:08 +0100

    :construction: Use delayed execution interactor

--- Toggl.Foundation.MvvmCross/ViewModels/TimeEntriesViewModel.cs
+++ Toggl.Foundation.MvvmCross/ViewModels/TimeEntriesViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading.Tasks;
@@ -16,26 +17,24 @@ using Toggl.Foundation.MvvmCross.Transformations;
 using Toggl.Foundation.MvvmCross.ViewModels.TimeEntriesLog;
 using Toggl.Foundation.MvvmCross.ViewModels.TimeEntriesLog.Identity;
 using Toggl.Foundation.Services;
-using Toggl.Foundation.Sync;
 using Toggl.Multivac;
 using Toggl.Multivac.Extensions;
 
 namespace Toggl.Foundation.MvvmCross.ViewModels
 {
-
     [Preserve(AllMembers = true)]
     public sealed class TimeEntriesViewModel
     {
-        private readonly ISyncManager syncManager;
         private readonly IInteractorFactory interactorFactory;
         private readonly IAnalyticsService analyticsService;
         private readonly ISchedulerProvider schedulerProvider;
 
         private readonly TimeEntriesGroupsFlattening groupsFlatteningStrategy;
 
-        private Subject<int?> timeEntriesPendingDeletionSubject = new Subject<int?>();
-        private IDisposable delayedDeletionDisposable;
-        private long[] timeEntriesToDelete;
+        private readonly Subject<int?> timeEntriesPendingDeletionSubject = new Subject<int?>();
+        private readonly HashSet<long> timeEntriesToDelete = new HashSet<long>();
+
+        private DelayedInteractorExecution<IObservable<Unit>> delayedDeletion;
 
         public IObservable<IEnumerable<AnimatableSectionModel<DaySummaryViewModel, LogItemViewModel, IMainLogKey>>> TimeEntries { get; }
         public IObservable<bool> Empty { get; }
@@ -48,7 +47,6 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
 
         public TimeEntriesViewModel(
             ITogglDataSource dataSource,
-            ISyncManager syncManager,
             IInteractorFactory interactorFactory,
             IAnalyticsService analyticsService,
             ISchedulerProvider schedulerProvider,
@@ -56,14 +54,12 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
             ITimeService timeService)
         {
             Ensure.Argument.IsNotNull(dataSource, nameof(dataSource));
-            Ensure.Argument.IsNotNull(syncManager, nameof(syncManager));
             Ensure.Argument.IsNotNull(interactorFactory, nameof(interactorFactory));
             Ensure.Argument.IsNotNull(analyticsService, nameof(analyticsService));
             Ensure.Argument.IsNotNull(schedulerProvider, nameof(schedulerProvider));
             Ensure.Argument.IsNotNull(rxActionFactory, nameof(rxActionFactory));
             Ensure.Argument.IsNotNull(timeService, nameof(timeService));
 
-            this.syncManager = syncManager;
             this.interactorFactory = interactorFactory;
             this.analyticsService = analyticsService;
             this.schedulerProvider = schedulerProvider;
@@ -100,15 +96,13 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
 
         public async Task FinalizeDelayDeleteTimeEntryIfNeeded()
         {
-            if (timeEntriesToDelete == null)
+            if (delayedDeletion != null)
             {
-                return;
-            }
+                await delayedDeletion?.ExecuteImmediately();
+                delayedDeletion = null;
 
-            delayedDeletionDisposable.Dispose();
-            await deleteTimeEntries(timeEntriesToDelete);
-            timeEntriesToDelete = null;
-            timeEntriesPendingDeletionSubject.OnNext(null);
+                timeEntriesPendingDeletionSubject.OnNext(null);
+            }
         }
 
         private IEnumerable<IGrouping<DateTime, IThreadSafeTimeEntry>> group(
@@ -124,49 +118,42 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
 
         private void delayDeleteTimeEntries(long[] timeEntries)
         {
-            timeEntriesToDelete = timeEntries;
+            if (delayedDeletion != null)
+            {
+                delayedDeletion.ExecuteImmediately().Subscribe();
+                analyticsService.DeleteTimeEntry.Track();
+            }
 
             timeEntriesPendingDeletionSubject.OnNext(timeEntries.Length);
+            timeEntriesToDelete.AddRange(timeEntries);
 
-            delayedDeletionDisposable = Observable.Merge( // If 5 seconds pass or we try to delete another TE
-                    Observable.Return(timeEntries).Delay(Constants.UndoTime, schedulerProvider.DefaultScheduler),
-                    timeEntriesPendingDeletionSubject
-                        .Where(numberOfDeletedTimeEntries => numberOfDeletedTimeEntries != null)
-                        .SelectValue(timeEntries)
-                )
-                .Take(1)
-                .SelectMany(deleteTimeEntries)
-                .Do(deletedTimeEntries =>
-                {
-                    // Hide bar if there isn't other TE trying to be deleted
-                    if (deletedTimeEntries == timeEntriesToDelete)
-                    {
-                        timeEntriesPendingDeletionSubject.OnNext(null);
-                    }
-                })
+            delayedDeletion = new DelayedInteractorExecution<IObservable<Unit>>(
+                interactorFactory.SoftDeleteMultipleTimeEntries(timeEntries),
+                Constants.UndoTime,
+                schedulerProvider.BackgroundScheduler);
+
+            delayedDeletion.Execute()
+                .Track(analyticsService.DeleteTimeEntry)
+                .Do(dismissUndo)
+                .Do(observable => observable.Subscribe())
                 .Subscribe();
         }
 
         private void cancelDeleteTimeEntry()
         {
-            timeEntriesToDelete = null;
-            delayedDeletionDisposable.Dispose();
+            delayedDeletion?.Cancel();
+            delayedDeletion = null;
+
+            dismissUndo();
+        }
+
+        private void dismissUndo()
+        {
+            timeEntriesToDelete.Clear();
             timeEntriesPendingDeletionSubject.OnNext(null);
         }
 
-        private IObservable<long[]> deleteTimeEntries(long[] timeEntries)
-        {
-            var observables =
-                interactorFactory.SoftDeleteMultipleTimeEntries(timeEntries)
-                    .Execute()
-                    .Track(analyticsService.DeleteTimeEntry);
-
-            return observables.SelectValue(timeEntries);
-        }
-
         private bool isNotRunning(IThreadSafeTimeEntry timeEntry) => !timeEntry.IsRunning();
-
-        private bool isNotDeleted(IThreadSafeTimeEntry timeEntry)
-            => timeEntriesToDelete == null || !timeEntriesToDelete.Contains(timeEntry.Id);
+        private bool isNotDeleted(IThreadSafeTimeEntry timeEntry) => !timeEntriesToDelete.Contains(timeEntry.Id);
     }
 }
