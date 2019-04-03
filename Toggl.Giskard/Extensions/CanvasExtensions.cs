b62a93d5a7d227886b9da035b22d3b90370b58b8
kornelijepetak <kornelije@gmail.com>
2019-04-03 20:51:55 +0200

    🚧 Add helper methods

---
+++ Toggl.Giskard/Extensions/CanvasExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Toggl.Giskard.Extensions
+{
+    public static class CanvasExtensions
+    {
+        public static Rect GetTextBounds(this Paint paint, string text)
+        {
+            var rect = new Rect();
+            paint.GetTextBounds(text, 0, text.Length, rect);
+            return rect;
+        }
+    }
+}
---
+++ Toggl.Multivac/Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Toggl.Multivac.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static int LastIndex<T>(this ICollection<T> self)
+            => self.Count - 1;
+
+        public static int LastIndex<T>(this IReadOnlyCollection<T> self)
+            => self.Count - 1;
+    }
+}
