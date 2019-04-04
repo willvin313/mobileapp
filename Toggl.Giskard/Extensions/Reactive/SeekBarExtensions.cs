3e69e54dc72189f36eb9c19bdeec801e52d629f6
kornelijepetak <kornelije@gmail.com>
2019-04-04 23:20:17 +0200

    🚧 Add seekbar rx bindings

---
+++ Toggl.Giskard/Extensions/Reactive/SeekBarExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+using Android.Text;
+using Android.Widget;
+using Java.Lang;
+using Toggl.Foundation.MvvmCross.Reactive;
+using static Android.Views.View;
+using static Android.Widget.SeekBar;
+
+namespace Toggl.Giskard.Extensions.Reactive
+{
+    public static class SeekBarExtensions
+    {
+        public static IObservable<int> Progress(this IReactive<SeekBar> reactive)
+            => Observable
+                .FromEventPattern<ProgressChangedEventArgs>(e => reactive.Base.ProgressChanged += e, e => reactive.Base.ProgressChanged -= e)
+                .Select(args => ((SeekBar)args.Sender).Progress)
+                .Do(progress => System.Diagnostics.Debug.WriteLine($"Seekbar emitted {progress}"));
+
+        public static Action<int> TextObserver(this IReactive<SeekBar> reactive, bool animate = true)
+            => progressValue => reactive.Base.SetProgress(progressValue, animate);
+
+    }
+}
